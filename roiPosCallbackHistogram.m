16ab11b2c4229b118081c5e07d8245821f4a183b
Michael Quinn <mquinn@mathworks.com>
2016-07-08 09:56:33 -0400

    added copyright and comments

--- roiPosCallbackHistogram.m
+++ roiPosCallbackHistogram.m
@@ -1,22 +1,35 @@
 function roiPosCallbackHistogram(pos, I, hLine)
+%
+% roiPosCallbackHistogram
+%
+% This callback function updates the histogram when the ROI (which is an
+% imrect object) is moved or resized.
+%
+% Written by Michael Quinn, PhD
+% michael.quinn@mathworks.com
+% 2016/04/27
+%
+%
+% version 1.0
+% 2016/04/27
+% Initial release
+%
+%
+
+% Copyright 2016 The MathWorks, Inc.
+
+% Crop out this ROI
 J = imcrop(I, pos);
+
+% Compute the histogram
 [C, B] = imhist(J);
+
+% Normalize the histogram
 C = C/numel(J);
+
+% Update the histogram plot with the new data
 set(hLine, 'XData', B);
 set(hLine, 'YData', C);
-axis(hLine.Parent, 'tight');
 
-% plo
-% M = mean2(J);
-% S = std2(J);
-% MN = double(min(J(:)));
-% MX = double(max(J(:)));
-% E = entropy(J);
-% 
-% fprintf('---------------------\n');
-% fprintf('[Min, Max]: [%.2f, %.2f]\n', MN, MX);
-% fprintf('Mean: %.2f\n', M);
-% fprintf('Std: %.2f\n', S);
-% fprintf('Entropy: %.2f\n', E);
-% 
-% 
+% Tighten the axes
+axis(hLine.Parent, 'tight');
